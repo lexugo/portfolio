58f8d24c29161eddb8346ecfb7c67dd89632709a
Hugo <dev@lexugo.ca>
2021-06-28 13:46:44 -0400

    Manizha · RUSSIAN WOMAN
    - Create structure inspired form Michiel de Graaf

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,19 @@
-import Error from 'next/error'
 import { memo } from 'react'
 
 function Welcome() {
     return (
-        <Error statusCode="Oops" title="This page is under development"/>
+        <div className="welcome">
+            <div className="signature"> 
+                <h2 className="name">Hugo Laquerre</h2>
+                <h3 className="title">Full-Stack developer</h3>
+            </div>
+            <div className="introduction">
+                <p>I make interfaces, shape brands and build digital products. Basically; I create, break and fix things, wrangle with code and design stuff.</p>
+            </div>
+            <div className="contact">
+                <a href="mailto:hireme@lexugo.ca">Let's work together!</a>
+            </div>
+        </div>
     )
 }
 
