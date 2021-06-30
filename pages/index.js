902cec74a10c1226a404d841154a7d65d8ecb8c7
Hugo <dev@lexugo.ca>
2021-06-30 11:49:56 -0400

    Supertramp · Goodbye Stranger
    - Add contact info to header

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
 import Signature from 'components/signature'
+import Contact from 'components/contact'
 
 function Welcome() {
     return (
         <header>
             <Signature />
+            <Contact />
         </header>
     )
 }
