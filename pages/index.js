b01c6c7562bdaa52145eddda232a9df4431b14b3
Hugo <dev@lexugo.ca>
2021-07-04 13:24:26 -0400

    Coolio · Gangsta's Paradise
    - Add title and description
    - Link to Ubisoft Montréal

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,15 @@
-import Error from "pages/_error"
+import Head from 'next/head'
 
 function Welcome() {
     return (
         <main className="welcome">
+            <Head>
+                <title>Hugo @lexugo</title>
+                <meta name="description" content="Hello; I'm Hugo. Full-Stack software engineer at Ubisoft." />
+            </Head>
             <div className="greeting">
                 <h1>Hello;</h1>
-                <h2>I'm Hugo. Full-Stack software engineer at Ubisoft.</h2>
+                <h2>I'm Hugo. Full-Stack software engineer at <a href="https://montreal.ubisoft.com/en/" target="_blank">Ubisoft</a>.</h2>
             </div>
         </main>
     )
