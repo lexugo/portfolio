e408317c435212bcd7589906f8015be8835df423
Hugo <dev@lexugo.ca>
2021-12-20 12:16:05 -0500

    Skinshape · Life as One
    - Create last.fm service
    - Create listening api endpoint to fetch latest track

---
+++ pages/api/listening.js
@@ -0,0 +1,7 @@
+import getListening from 'services/music'
+
+export default async function test(req, res) {
+	const track = await getListening()
+
+	res.status(200).json(track) // Todo: Error handling
+}
